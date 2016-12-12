eb676906be940d1d609eb6f321d6e9a9add10466
Edward Dibbs <EdDibbs@gmail.com>
2016-12-11 20:30:25 -0800

    Rewrote everything. DateTime is now used with for the classification, wind direction has been removed from data samples because it's not needed. Site names are parsed so that we know what to use as our Y (target) columns.

--- Part2/main.py
+++ Part2/main.py
@@ -1,23 +1,90 @@
 import numpy as np
+import matplotlib.dates as mdates
+from sklearn import svm
 
-rawdata = ""
-print "Opening TrainingData"
+# siteNameDictionary maps col# to Dictionary Name
+siteNameDictionary = {}
 
-dataPoints = 0
-with open("TrainingData.csv") as fi:
-    for line in fi:
-        # just ignore lines where we're missing some data from one or more sites
-        if (line.count(",,") > 0) or line.count(",") < 27:
-            continue
-        rawdata += line
-        dataPoints += 1
+def strpdate2num(fmt):
+    def converter(b):
+        return mdates.strpdate2num(fmt)(b.decode('ascii'))
+    return converter
 
-with open("temp_preprocessed.csv", 'w') as fi:
-    fi.write(rawdata)
+def preprocess(inputFile, isTrainData):
+    rawdata = ""
+    outputFile = "temp_" + inputFile
+    x = []
+    y1 = []
+    y2 = []
+    y3 = []
+    
+    with open(inputFile) as fi:
+        # outputDictionary maps '(dateTimeStamp)' to measures of all sites wind velocity
+        for lineIndex, line in enumerate(fi):
+            if lineIndex == 0 and isTrainData:
+                # we're on the first line, let's parse out site names
+                for index, col in enumerate(line.split(',')):
+                    if col == '':
+                        continue
+                    if col == "Site Name:":
+                        continue
+                    
+                    siteNameDictionary[index] = col
+                
+                continue # done parsing site names, continue to the next line
+                
+            if lineIndex < 9:
+                continue
+            
+            # just ignore lines where we're missing some data from one or more sites
+            if (line.count(",,") > 0) or line.count(",") < 14:
+                continue
+            
+            rawdata += line                
+                
+        
+    with open(outputFile, 'w') as fi:
+        fi.write(rawdata)
 
-print "Done preprocessing. Loaded", dataPoints, "datapoints. Loading via numpy..."
+    date_converter = strpdate2num('%m/%d/%Y %H:%M:%S %p')
+    colsX = []
+    colsY = []
+      
+    
+    if (isTrainData):
+        colsX.append(0) #datetime
+        for col, site in siteNameDictionary.items():
+            if site == "Snake Range West Pinyon-Juniper" or site == "Snake Range West Subalpine" or site == "Snake Range West Montane":
+                colsY.append(col)
+            else:
+                colsX.append(col)
+        print ("Using the following cols for X: ", colsX)
+        print ("Using the following cols for Y: ", colsY)
+        colsX = tuple(colsX)
+        colsY = tuple(colsY)
+        
+    else: # test data
+        colsX = tuple(0,)
+        
+    x = np.loadtxt(outputFile, delimiter=',', usecols=colsX, converters={0:date_converter})
+    
+    if (isTrainData):
+        Y = np.loadtxt(outputFile, delimiter=",", usecols=colsY)
+        
+    print ("Done preprocessing ", inputFile)
+      
+    return x, Y
 
-trainingX = np.loadtxt("temp_preprocessed.csv", delimiter=',', skiprows=1, usecols=(1, 3, 9, 11, 17, 19, 25, 27))
-trainingY = np.loadtxt("temp_preprocessed.csv", delimiter=",", skiprows=1, usecols=(5, 7, 13, 15, 21, 23))
+print ("Opening TrainingData")
+trainingX, trainingY = preprocess("TrainingData.csv", True)
+print ("Done loading.")
 
-print "Done loading."
+print ("Opening TestData")
+#testX = preprocess("TestData.csv", False) #doesn't work currently
+print ("Done loading.")
+
+print("SVM Fitting...");
+clf = svm.SVR()
+clf.fit(trainingX, trainingY)  
+print("Done.");
+
